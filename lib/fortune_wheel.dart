f98a7bded17f927bc492511d1284bade233229f1
AbdurahmanAlmehdi <abdurahman.i.work@gmail.com>
2026-01-13 20:41:58 +0200

    refactor: add home page

--- lib/fortune_wheel.dart
+++ lib/fortune_wheel.dart
@@ -1,13 +1,10 @@
-// Models
+
 export 'src/models/slice.dart';
 export 'src/models/content_type.dart';
 
-// Configuration
 export 'src/configuration/wheel_configuration.dart';
 export 'src/configuration/pin_configuration.dart';
 
-// Widgets
 export 'src/widgets/fortune_wheel.dart';
 
-// Utils (for advanced usage)
 export 'src/utils/wheel_math.dart';
